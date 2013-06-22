39fdea6a079e35da582f7714aad5d549676264d0
Nevo Segal <nevosegal@gmail.com>
2013-06-22 19:18:08 +0100

    fixed Codebird

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,25 @@
 var cb = new Codebird;
 cb.setConsumerKey('ntRLHzCjKiDsSaEXCJ30A', '2R1Im1hENmkKgf6YKRRt9VE0ejvJrDeaUxhBId7VY');
 cb.__call(
-'oauth_requestToken',
-{oauth_callback: 'oob'},
-function (reply) {
-    // stores it
-    cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+    'oauth_requestToken',
+    {oauth_callback: 'oob'},
+    function (reply) {
+        // stores it
+        cb.setToken(reply.oauth_token, reply.oauth_token_secret);
 
-    // gets the authorize screen URL
-    cb.__call(
-        'oauth_authorize',
-        {},
-        function (auth_url) {
-            window.codebird_auth = window.open(auth_url);
-        }
-    );
-}
-));
-
-
-
+        // gets the authorize screen URL
+        cb.__call(
+            'oauth_authorize',
+            {},
+            function (auth_url) {
+                window.codebird_auth = window.open(auth_url);
+            }
+        );
+    }
+);
 
 $(document).keypress(function(event) {
   if(event.keyCode == 115){
-  	document.getElementById('tada').play();
+    document.getElementById('tada').play();
   }
 });
