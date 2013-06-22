47ee1bcd047c1d81945f543217dba7e9766f47a3
Hugh Rawlinson <hugh@codeoclock.net>
2013-06-22 19:29:06 +0100

    no message

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,26 @@
 */
 var cb = new Codebird;
 cb.setConsumerKey('ntRLHzCjKiDsSaEXCJ30A', '2R1Im1hENmkKgf6YKRRt9VE0ejvJrDeaUxhBId7VY');
-cb.__call(
-    'oauth_requestToken',
-    {oauth_callback: 'oob'},
-    function (reply) {
-        // stores it
-        cb.setToken(reply.oauth_token, reply.oauth_token_secret);
 
-        // gets the authorize screen URL
-        cb.__call(
-            'oauth_authorize',
-            {},
-            function (auth_url) {
-                window.codebird_auth = window.open(auth_url);
-            }
-        );
-    }
-);
+function authTwit(){
+	cb.__call(
+	    'oauth_requestToken',
+	    {oauth_callback: 'oob'},
+	    function (reply) {
+	        // stores it
+	        cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+
+	        // gets the authorize screen URL
+	        cb.__call(
+	            'oauth_authorize',
+	            {},
+	            function (auth_url) {
+	                window.codebird_auth = window.open(auth_url);
+	            }
+	        );
+	    }
+	);
+}
 
 $(document).keypress(function(event) {
   if(event.keyCode == 115){
